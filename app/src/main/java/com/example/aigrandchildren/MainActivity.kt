d1400c5ef994a24f910d641ee757053d4904e073
gic_Han <gichan4444@gmail.com>
2023-05-04 16:38:22 +0900

    chatGPT setting

--- app/src/main/java/com/example/aigrandchildren/MainActivity.kt
+++ app/src/main/java/com/example/aigrandchildren/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
@@ -16,22 +15,26 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.aigrandchildren.adapter.MessageAdapter
 import com.example.aigrandchildren.model.Message
-import okhttp3.OkHttpClient
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
-    var recycler_view: RecyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-    var tv_welcome: TextView = findViewById<TextView>(R.id.tv_welcome)
-    var et_msg: EditText = findViewById<EditText>(R.id.et_msg)
-    var btn_send: ImageButton = findViewById<ImageButton>(R.id.btn_send)
-    var btn_voice: ImageButton = findViewById<ImageButton>(R.id.btn_voice)
-    var activityResultLauncher: ActivityResultLauncher<Intent>? = null
-    var messageList: MutableList<Message>? = null
-    var messageAdapter: MessageAdapter? = null
-    var client: OkHttpClient? = null
+    private var recycler_view: RecyclerView = findViewById(R.id.recycler_view)
+    private var tv_welcome: TextView = findViewById(R.id.tv_welcome)
+    private var et_msg: EditText = findViewById(R.id.et_msg)
+    private var btn_send: ImageButton = findViewById(R.id.btn_send)
+    private var btn_voice: ImageButton = findViewById(R.id.btn_voice)
+    private var activityResultLauncher: ActivityResultLauncher<Intent>? = null
+    private var messageList: MutableList<Message>? = null
+    private var messageAdapter: MessageAdapter? = null
+    private var client: OkHttpClient? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,20 +42,21 @@ class MainActivity : AppCompatActivity() {
         val manager = LinearLayoutManager(this)
         manager.stackFromEnd = true
         recycler_view.layoutManager = manager
-        messageList = ArrayList<Message>()
+        messageList = ArrayList()
         messageAdapter = MessageAdapter(messageList as ArrayList<Message>)
         recycler_view.adapter = messageAdapter
-        btn_send.setOnClickListener( View.OnClickListener // 챗봇에게 질문하려면 버튼을 눌러야 한다
-            {
-                val question = et_msg.text.toString().trim { it <= ' ' }
-                addToChat(question, Message.SENT_BY_ME)
-                et_msg.setText("")
-                callAPI(question)
-                tv_welcome.visibility = View.GONE
-            })
+
+        // 버튼을 눌러 채팅 입력
+        btn_send.setOnClickListener {
+            val question = et_msg.text.toString().trim { it <= ' ' }
+            addToChat(question, Message.SENT_BY_ME)
+            et_msg.setText("")
+            callAPI(question)
+            tv_welcome.visibility = View.GONE
+        }
 
         // startActivityForResult가 Deprecated되서 registerForActivityResult를 사용
-        activityResultLauncher = registerForActivityResult<Intent, ActivityResult>(
+        activityResultLauncher = registerForActivityResult(
             ActivityResultContracts.StartActivityForResult()
         ) { result ->
             if (result.resultCode == RESULT_OK) {
@@ -61,19 +65,17 @@ class MainActivity : AppCompatActivity() {
                 et_msg.setText(resultData!![0])
             }
         }
-        btn_voice.setOnClickListener(
-            View.OnClickListener
-            // 음성인식을 하려면 버튼을 눌러야 한다
-            {
-                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
-                intent.putExtra(
-                    RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
-                )
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko_KR")
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "음성인식을 시작합니다.")
-                activityResultLauncher!!.launch(intent)
-            })
+        // 버튼을 눌러 음성인식
+        btn_voice.setOnClickListener {
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko_KR")
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "음성인식을 시작합니다.")
+            activityResultLauncher!!.launch(intent)
+        }
         client = OkHttpClient().newBuilder()
             .connectTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(120, TimeUnit.SECONDS)
@@ -94,7 +96,7 @@ class MainActivity : AppCompatActivity() {
         messageList!!.removeAt(messageList!!.size - 1)
         addToChat(response, Message.SENT_BY_BOT)
     }
-// 할일 - 밑에 수정해야 함, TTS 넣기, 이전 대화 저장 및 적용
+// 할일 - TTS 넣기, 이전 대화 저장 및 적용
     fun callAPI(question: String?) {
         //okhttp
         messageList!!.add(Message("...", Message.SENT_BY_BOT))
@@ -121,10 +123,10 @@ class MainActivity : AppCompatActivity() {
         } catch (e: JSONException) {
             e.printStackTrace()
         }
-        val body: RequestBody = RequestBody.create(`object`.toString(), JSON)
-        val request: Request = Builder()
+        val body: RequestBody = `object`.toString().toRequestBody(JSON)
+        val request: Request = Request.Builder()
             .url("https://api.openai.com/v1/chat/completions")
-            .header("Authorization", "Bearer " + MY_SECRET_KEY)
+            .header("Authorization", "Bearer $MY_SECRET_KEY")
             .post(body)
             .build()
         client!!.newCall(request).enqueue(object : Callback {
@@ -135,12 +137,10 @@ class MainActivity : AppCompatActivity() {
             @Throws(IOException::class)
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful) {
-                    var jsonObject: JSONObject? = null
+                    val jsonObject = JSONObject(response.body!!.string())
                     try {
-                        jsonObject = JSONObject(response.body!!.string())
                         val jsonArray = jsonObject.getJSONArray("choices")
-                        val result =
-                            jsonArray.getJSONObject(0).getJSONObject("message").getString("content")
+                        val result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content")
                         addResponse(result.trim { it <= ' ' })
                     } catch (e: JSONException) {
                         e.printStackTrace()
@@ -153,7 +153,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        val JSON: MediaType = get.get("application/json; charset=utf-8")
+        val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
         private const val MY_SECRET_KEY = "{Your Secret Key}"
     }
 }
--- app/src/main/java/com/example/aigrandchildren/adapter/MessageAdapter.kt
+++ app/src/main/java/com/example/aigrandchildren/adapter/MessageAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.aigrandchildren.adapter
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +18,7 @@ class MessageAdapter(messageList: List<Message>) :
         this.messageList = messageList
     }
 
+    @SuppressLint("InflateParams")
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
